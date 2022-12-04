a5e972b406f8c6d1dc9a7aea7ca768b5897f0fef
CatLe2003 <119800521+CatLe2003@users.noreply.github.com>
2022-12-05 01:53:17 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,31 +15,12 @@ int main()
 	//Name of the file to be sorted
 	string inputFile;
 	inputFile = "Books_rating.csv";
-	/*ifstream ifs = openFiletoRead(inputFile);
 
-	string line;
-	getline(ifs, line);
-	Book* a = new Book[1000000];
-
-	for (int i = 0; i < 1000000; ++i)
-	{
-		getline(ifs, line);
-		Book temp = readBook(line);
-		a[i] = temp;
-	}
-
-	ofstream ofs = openFiletoWrite("Books_rating.txt");
-
-	for (int i = 0; i < 1000000; ++i)
-	{
-		ofs << a[i].id << "," << a[i].other_info << endl;
-	}*/
 	//Name of the Result File
 	string outputFile;
 	outputFile = "sorted_books_rating.csv";
 
-	//externalSort(inputFile, outputFile, numberofElements, numberofFiles);
-	mergeFiles(outputFile, numberofElements, numberofFiles);
-
+	externalSort(inputFile, outputFile, numberofElements, numberofFiles);
+	
 	return 0;
 }
